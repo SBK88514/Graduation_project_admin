76eb6afd233d36480f5b6558d023013c0a37ab23
sbk88514 <sbk88514@gmail.com>
2024-12-13 00:28:53 +0200

    paginaiton in process

--- src/components/pages/privatePages/AllEmployees.jsx
+++ src/components/pages/privatePages/AllEmployees.jsx
@@ -3,12 +3,33 @@ import EmployeesTabl from "../tables/employees/EmployeesTable";
 import Header from "../../ui/Header";
 import useFetch from "../../hooks/useFetch";
 import NotEmployees from "./NotEmployees";
+// import Paginaiton from "../../ui/Paginaiton";
+// import { useQuery } from "@tanstack/react-query";
 
 function AllEmployees() {
   const [data, isLoading, isError] = useFetch(
     "/users/employee/getallempolyees"
   );
 
+  // const [page, setPage] = useState(1)
+  // const [limit] = useState(2)
+
+  // const url = `/users/employee/getallempolyees?page=${page}&limit=${limit}`;
+
+
+  // const {data, isLoading, isError, error} = useQuery({
+  //   queryKey: ["get_employees", page],
+  //   queryFn: async () => await axios.get(url),
+  //   select: (data) => ({
+  //     allEmployees: data.allEmployees,
+  //     count: data.count,
+  //   }),
+    
+  // });
+
+
+  //   const [employees, setEmployees] = useState([])
+  //   const [togle,setTogle] = useState(false)
 
  
 
@@ -24,6 +45,16 @@ function AllEmployees() {
         <EmployeesTabl employees={data} />
       )}
       {/* {data && data.length && <EmployeesTabl employees={data} />} */}
+
+      {/* {isError && <div>{error}</div>}
+      {data && !data.allEmployees?.length ? (
+        <NotEmployees />
+      ) : (
+        <EmployeesTabl employees={data.allEmployees} />
+      )} */}
+      {/* {data && data.length && <EmployeesTabl employees={data} />} */}
+      {/* {Pagination} */}
+      {/* <Paginaiton employeesLength={data?.count} limit={limit} setPage={setPage}/> */}
     </div>
   );
 }
