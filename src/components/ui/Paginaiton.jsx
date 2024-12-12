c5db789f18b9873535b41282ebcf338b8c8eaf8c
sbk88514 <sbk88514@gmail.com>
2024-12-13 00:31:24 +0200

    Pagination in process 1

---
+++ src/components/ui/Paginaiton.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Paginaiton({employeesLength, limit, setPage}) {
+  const pages = Math.ceil(employeesLength / limit);
+  const btns = [];
+  for (let i = 1; i < pages + 1; i++) btns.push(i);
+  console.log(btns);
+
+  return (
+    <div>
+      <nav
+        aria-label="Page navigation example"
+        className="flex justify-center items-center mt-5"
+      >
+        <ul className="inline-flex -space-x-px text-sm">
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              Previous
+            </a>
+          </li>
+          {btns.map((btn) => (
+            <li key={btn}>
+              <Link
+                onClick={()=> setPage(btn)}
+                className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                {btn}
+              </Link>
+            </li>
+          ))}
+
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Paginaiton;
+
+{
+  /* <li>
+      <a href="#" aria-current="page" className="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+    </li> */
+}
